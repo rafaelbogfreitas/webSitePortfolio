df78835b918d9da845d5cbcd88ee207daff5e479
Rafael Freitas <rafaelborges1985@hotmail.com>
2019-04-29 13:44:50 +0100

    waiting on content

--- app/js/script.js
+++ app/js/script.js
@@ -2,21 +2,22 @@ $(document).ready(function(){
   //Hides the main content, leaving only the intro session displayed
   $('.content').hide();
 
+  //creates an underline on the nav item when pages is selected
   $('#main-nav li').on('click', function (event){
-    $(event.currentTarget).css({'text-decoration': 'underline'}).siblings()
-    .css({'text-decoration':'none'});
+    $(event.currentTarget).css({'text-decoration': 'underline', 'color':'orange'}).siblings()
+    .css({'text-decoration':'none', 'color':'black'});
   });
 
   //Intro button 'click' event listener
   $('#intro-btn').on('click', function(){
-    $('.container').slideDown(200);
+    $('.container').show();
     $('.content').hide();
     $('.about').show();
   });
 
   //About button 'click' event listener
   $('#about-btn').on('click', function(){
-    $('.container').slideUp(200);
+    $('.container').hide();
     $('.content').show();
     $('.about').show();
     $('.projects').hide();
